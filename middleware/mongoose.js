3a94af4f6cdbaf94dea866c43c7f30c5020b024a
Varinder Brar <103635395+Brar21@users.noreply.github.com>
2023-10-18 20:15:08 +0530

    new update

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -2,3 +2,4 @@ const mongoose=require("mongoose")
 
 const connection=mongoose.connect('mongodb+srv://varinder:varinderbrar@cluster0.jy11wkr.mongodb.net/test?retryWrites=true&w=majority')
 module.exports={connection}
+//mongosh "mongodb+srv://cluster0.jy11wkr.mongodb.net/" --apiVersion 1 --username varinder
